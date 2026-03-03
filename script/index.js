2cb826b7d5d0067ab0d0eea05ada85a6edc60240
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-03-03 13:07:44 +0600

    load words by api in lessons section

--- script/index.js
+++ script/index.js
@@ -1,8 +1,10 @@
+// Function => lessons button load
+
 const loadLessons = () => {
     fetch('https://openapi.programming-hero.com/api/levels/all')
         .then(res => res.json())
         .then(json => displayLevels(json.data));
-}
+    }
 const displayLevels = (lessons) => {
     // get the parent lessons div
     const lessonsContainer = document.getElementById('lessons-container');
@@ -12,7 +14,7 @@ const displayLevels = (lessons) => {
         // create btn for all lesson 
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = `
-            <button href="" class="btn btn-outline btn-primary">
+            <button onclick="loadWords(${lesson.level_no})" class="btn btn-outline btn-primary">
                 <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
             </button>
         `;
@@ -21,3 +23,26 @@ const displayLevels = (lessons) => {
     }
 }
 loadLessons();
+
+
+// words by api
+const loadWords = (id) => {
+
+const url = `https://openapi.programming-hero.com/api/level/${id}`;
+fetch(url)
+    .then(res => res.json())
+    .then(json => displayWords(json.data));
+}
+
+const displayWords = (words) => {
+    const wordsContainer = document.getElementById('word-container');
+    wordsContainer.innerHTML = '';
+    words.forEach((word) => {
+        const wordCard = document.createElement('div');
+        wordCard.innerHTML = `
+            <h2>Hello</h2>
+        `;
+        wordsContainer.append(wordCard);
+    })
+}
+
